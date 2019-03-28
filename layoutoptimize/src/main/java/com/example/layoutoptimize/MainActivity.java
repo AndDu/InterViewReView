6289079a843bdcc3466ef2b2b11f03be31e1fe81
AndDu <694495047@qq.com>
2019-03-28 09:04:32 +0800

    布局优化项目提交

---
+++ layoutoptimize/src/main/java/com/example/layoutoptimize/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.layoutoptimize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.debug.hv.ViewServer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewServer.get(this).addWindow(this);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ViewServer.get(this).setFocusedWindow(this);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ViewServer.get(this).removeWindow(this);
+    }
+}
